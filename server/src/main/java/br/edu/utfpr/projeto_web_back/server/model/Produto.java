cf2f4100a34cb1c0e29a6c7d8e7610dffad8c2c5
viniciusflyssak <viniciusflyssak@gmail.com>
2024-04-03 22:48:44 -0300

    Adicionadas propriedades da classe produto

--- server/src/main/java/br/edu/utfpr/projeto_web_back/server/model/Produto.java
+++ server/src/main/java/br/edu/utfpr/projeto_web_back/server/model/Produto.java
@@ -1,4 +1,29 @@
 package br.edu.utfpr.projeto_web_back.server.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "PRODUTOS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduto;
+
+    private String nome;
+
+    private String descricao;
+
+    private BigDecimal preco;
+
+    @ManyToOne
+    private Categoria categoria;
 }
