3527e55ab9782ed2676d845aac577cbb2d0f2d97
viniciusflyssak <viniciusflyssak@gmail.com>
2024-04-10 22:16:15 -0300

    Adicionadas as propriedades da itens pedido

--- server/src/main/java/br/edu/utfpr/projeto_web_back/server/model/ItensPedido.java
+++ server/src/main/java/br/edu/utfpr/projeto_web_back/server/model/ItensPedido.java
@@ -1,4 +1,24 @@
 package br.edu.utfpr.projeto_web_back.server.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "ITENS_PEDIDO")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ItensPedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPedido;
+
+    private BigDecimal preco;
+
+    @ManyToOne
+    private Produto produto;
 }
