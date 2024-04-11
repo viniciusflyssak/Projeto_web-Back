3eb5382711962d72f4bd1197d1f7a6d0468f7560
DavidMartinello <123309925+DavidMartinello@users.noreply.github.com>
2024-04-10 22:30:31 -0300

    Criação dos itens das classes pedido e usuario

--- server/src/main/java/br/edu/utfpr/projeto_web_back/server/model/Pedido.java
+++ server/src/main/java/br/edu/utfpr/projeto_web_back/server/model/Pedido.java
@@ -1,4 +1,30 @@
 package br.edu.utfpr.projeto_web_back.server.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "PEDIDO")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Long valor;
+
+    @NotNull
+    private LocalDate data;
+
+    @ManyToOne
+    private Usuario usuario;
 }
--- server/src/main/java/br/edu/utfpr/projeto_web_back/server/model/Usuario.java
+++ server/src/main/java/br/edu/utfpr/projeto_web_back/server/model/Usuario.java
@@ -1,5 +1,40 @@
 package br.edu.utfpr.projeto_web_back.server.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Table(name = "USUARIO")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String nome;
+
+    @NotNull
+    private String nomeExibicao;
+
+    @NotNull
+    @Email(message = "Insira um email válido")
+    private String email;
+
+    @NotNull
+    @Size(min = 6)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "Senha deve conter ao menos uma letra maiúscula, minúscula e um número")
+    private String senha;
+
+
 }
 
